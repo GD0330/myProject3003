320f5c6e80ebd671edcc255cf18d73a213cf87bc
GD0330 <161241619+GD0330@users.noreply.github.com>
2024-04-21 15:20:06 +0300

    Update OpenCommand.java

--- OpenCommand.java
+++ OpenCommand.java
@@ -1,14 +1,30 @@
-package bg.tu_varna.sit.a1.f22621658.Files;
+package bg.tu_varna.sit.a1.f22621658.fileCommands;
 
-import bg.tu_varna.sit.a1.f22621658.Files.Command;
-import bg.tu_varna.sit.a1.f22621658.Files.Filework;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class OpenCommand implements Command {
         @Override
-        public String execute(Filework filework, Scanner scanner, String content) throws IOException {
-            return filework.OpenFile(scanner.next());
+        public String execute(String filename, Scanner scanner, String content) throws IOException {
+            File file = new File(filename);
+            if (!file.exists()) {
+                if (file.createNewFile()) {
+                    System.out.println("File created successfully.");
+                } else {
+                    throw new IOException("Failed to create the file.");
+                }
+            }
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            String line;
+            StringBuilder rawContent = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                rawContent.append(line).append("\n");
+            }
+            reader.close();
+            System.out.println("File opened successfully.");
+            return rawContent.toString();
         }
 }
