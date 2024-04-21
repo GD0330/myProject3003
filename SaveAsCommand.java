8d106417c472165c6cb30509a0bace14055c63e4
GD0330 <161241619+GD0330@users.noreply.github.com>
2024-04-21 15:20:31 +0300

    Update SaveAsCommand.java

--- SaveAsCommand.java
+++ SaveAsCommand.java
@@ -1,15 +1,21 @@
-package bg.tu_varna.sit.a1.f22621658.Files;
+package bg.tu_varna.sit.a1.f22621658.fileCommands;
 
-import bg.tu_varna.sit.a1.f22621658.Files.Command;
-import bg.tu_varna.sit.a1.f22621658.Files.Filework;
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class SaveAsCommand implements Command {
-    public String execute(Filework filework, Scanner scanner, String content) throws IOException {
+    public String execute(String filename, Scanner scanner, String content) throws IOException {
         String newFileName = scanner.next();
-        filework.SaveAsFile(content, newFileName);
-        return content; // Return the content after saving
+        if (filename == null) {
+            System.out.println("No file is currently opened.");
+            return null;
+        }
+        BufferedWriter writer = new BufferedWriter(new FileWriter(newFileName));
+        writer.write(content);
+        writer.close();
+        System.out.println("Changes saved successfully to " + newFileName);
+        return content;
     }
 }
