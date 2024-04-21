ae62a8d1448bcf76abff5ff01aa5d76a677c4d21
GD0330 <161241619+GD0330@users.noreply.github.com>
2024-04-21 15:18:36 +0300

    Update Command.java

--- Command.java
+++ Command.java
@@ -1,10 +1,8 @@
-package bg.tu_varna.sit.a1.f22621658.Files;
-
-import bg.tu_varna.sit.a1.f22621658.Files.Filework;
+package bg.tu_varna.sit.a1.f22621658.fileCommands;
 
 import java.io.IOException;
 import java.util.Scanner;
 
 public interface Command {
-    String execute(Filework filework, Scanner scanner, String content) throws IOException;
+    String execute(String filename, Scanner scanner, String content) throws IOException;
 }
