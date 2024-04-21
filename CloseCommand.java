5f2febdcc8dbca0760815332c4a09af697b91da9
GD0330 <161241619+GD0330@users.noreply.github.com>
2024-04-21 15:18:12 +0300

    Update CloseCommand.java

--- CloseCommand.java
+++ CloseCommand.java
@@ -1,10 +1,14 @@
-package bg.tu_varna.sit.a1.f22621658.Files;
+package bg.tu_varna.sit.a1.f22621658.fileCommands;
 
 import java.util.Scanner;
 
 public class CloseCommand implements Command {
-    public String execute(Filework filework, Scanner scanner, String content) {
-        filework.CloseFile();
+    public String execute(String filename, Scanner scanner, String content) {
+        if (filename == null ) {
+            System.out.println("No file is currently opened.");
+        }
+        System.out.println("File closed successfully.");
         return null;
     }
+
 }
