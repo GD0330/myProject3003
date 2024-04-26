d9e2c8a27b24cbbd2f68acd8d895ec049745be81
GD0330 <161241619+GD0330@users.noreply.github.com>
2024-04-26 09:57:23 +0300

    Update SimpleContentHandler.java

--- SimpleContentHandler.java
+++ SimpleContentHandler.java
@@ -10,8 +10,8 @@ import java.util.Map;
 
 public class SimpleContentHandler implements ContentHandler{
     @Override
-    public Calendar decode(String content) {
-        Calendar calendar=new Calendar();
+    public Calendars decode(String content) {
+        Calendars calendar=new Calendars();
         String[] allEvents =content.split("#");
         String[] singleEventInfo;
         String[] eventDateAndName;
@@ -25,25 +25,36 @@ public class SimpleContentHandler implements ContentHandler{
             int k=3;
             Map<Integer, Seat> seats= new HashMap<>();
 
-            for (int row = 1; row <= Integer.valueOf(hallInfo[1]); row++) {
-                for (int seatNum = 1; seatNum <= Integer.valueOf(hallInfo[2]); seatNum++) {
-                    Seat seat = new Seat(row, seatNum);
-                    seat.setSeatState(hallInfo[k]);
-                    seats.put(seatNum, seat);
-                    k++;
-                }
+            int r=Integer.parseInt(hallInfo[1]);
+            int sN=Integer.parseInt(hallInfo[2]);
+            
+            Hall hall = new Hall(Integer.valueOf(hallInfo[0]));
+
+            for (int j = 1; j < hall.getNumRows()*hall.getNumSeatsPerRow()+1; j++) {
+                hall.getSeats().get(j).setSeatState(hallInfo[k]);
+                k++;
             }
-            Hall hall = new Hall(Integer.valueOf(hallInfo[0]),Integer.valueOf(hallInfo[1]),Integer.valueOf(hallInfo[2]),seats);
 
             Event event= new Event(eventDateAndName[0],eventDateAndName[1],hall);
 
             calendar.addEvent(event);
         }
-        return new Calendar();
+        return calendar;
     }
-
     @Override
-    public String encode(String content) {
-        return null;
+    public String encode(Calendars calendars) {
+        StringBuilder strB=new StringBuilder();
+        for (int i = 0; i < calendars.getEvents().size(); i++) {
+            strB.append(calendars.getEvents().get(i).getDate()+ ";"+
+                    calendars.getEvents().get(i).getName()+ ";]");
+            strB.append(calendars.getEvents().get(i).getHall().getNumber()+";"+
+                    calendars.getEvents().get(i).getHall().getNumRows()+";"+
+                    calendars.getEvents().get(i).getHall().getNumSeatsPerRow()+";");
+            for (int j = 1; j < calendars.getEvents().get(i).getHall().getNumRows()* calendars.getEvents().get(i).getHall().getNumSeatsPerRow()+1; j++) {
+                strB.append(calendars.getEvents().get(i).getHall().getSeats().get(j).getSeatState()+";");
+            }
+            strB.append("#");
+        }
+        return strB.toString();
     }
 }
