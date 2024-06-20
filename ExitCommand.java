b6b08aebca6a185d457a537399b42b3a26ee4f00
GD0330 <161241619+GD0330@users.noreply.github.com>
2024-06-20 13:43:54 +0300

    Update ExitCommand.java

--- ExitCommand.java
+++ ExitCommand.java
@@ -1,10 +1,8 @@
 package bg.tu_varna.sit.a1.f22621658.fileCommands;
 
-import java.util.Scanner;
-
 public class ExitCommand implements Command {
-    public String execute(String filename, Scanner scanner, String content) {
-        System.out.println("Exiting...");
+    public Calendars execute( String[] param, Calendars calendars) {
+        System.out.println("Exiting the program...");
         return null;
     }
 }
