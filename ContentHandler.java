ca0e646c196be87b2cfa652e2bd6c2f557e14ac6
GD0330 <161241619+GD0330@users.noreply.github.com>
2024-06-20 14:05:21 +0300

    Delete ContentHandler.java

--- ContentHandler.java
+++
@@ -1,8 +0,0 @@
-package bg.tu_varna.sit.a1.f22621658.fileCommands;
-
-import bg.tu_varna.sit.a1.f22621658.models.Calendar;
-
-public interface ContentHandler {
-    String encode(String content);
-    Calendar decode(String content);
-}
