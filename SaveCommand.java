1dadd6800bf068e8ef839016ba3df8a0005efa77
GD0330 <161241619+GD0330@users.noreply.github.com>
2024-06-20 13:54:13 +0300

    Update SaveCommand.java

--- SaveCommand.java
+++ SaveCommand.java
@@ -3,18 +3,18 @@ package bg.tu_varna.sit.a1.f22621658.fileCommands;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class SaveCommand implements Command {
-    public String execute(String filename, Scanner scanner, String content) throws IOException {
-        if (filename == null) {
+    public Calendars execute( String[] param, Calendars calendars) throws IOException {
+        if (OpenCommand.filename == null) {
             System.out.println("No file is currently opened.");
             return null;
         }
-        BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
-        writer.write(content);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(OpenCommand.filename));
+        SimpleContentHandler simpleContentHandler=new SimpleContentHandler();
+        writer.write(simpleContentHandler.encode(calendars));
         writer.close();
         System.out.println("Changes saved successfully.");
-        return content;
+        return calendars;
     }
 }
