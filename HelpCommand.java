cf329e8a249fe6ce48bb752a267cc3863152e964
GD0330 <161241619+GD0330@users.noreply.github.com>
2024-06-20 13:45:37 +0300

    Update HelpCommand.java

--- HelpCommand.java
+++ HelpCommand.java
@@ -1,28 +1,30 @@
 package bg.tu_varna.sit.a1.f22621658.fileCommands;
 
-import java.util.Scanner;
-
 public class HelpCommand implements Command {
-    public String execute(String filename, Scanner scanner, String content) {
+    public Calendars execute( String[] param, Calendars calendars) {
         System.out.println("The following commands are supported:");
-        System.out.println("open <file>    - opens <file>");
-        System.out.println("close          - closes currently opened file");
-        System.out.println("save           - saves the currently open file");
-        System.out.println("saveas <file>  - saves the currently open file in <file>");
-        System.out.println("help           - prints this information");
-        System.out.println("exit           - exits the program");
-        System.out.println("addevent       - adds new event");
-        System.out.println("<date> <name>");
-        System.out.println("freeseats      - checks which seats are Free");
-        System.out.println("book <name>    - books a seat ");
+        System.out.println("open <file>              -> opens <file>");
+        System.out.println("close                    -> closes currently opened file");
+        System.out.println("save                     -> saves the currently open file");
+        System.out.println("saveas <file>            -> saves the currently open file in <file>");
+        System.out.println("help                     -> prints this information");
+        System.out.println("exit                     -> exits the program");
+        System.out.println("addevent <date> <name>   -> adds new event");
+        System.out.println("<hall>");
+        System.out.println("freeseats <name> <date>  -> checks which seats are Free");
+        System.out.println("book <name>              -> books a seat ");
         System.out.println("<date> <row>");
         System.out.println("<seat> <note>");
-        System.out.println("unbook <name>  - cancel a booked seat");
-        System.out.println("<date> <row>");
-        System.out.println("<seat>");
-        System.out.println("buy <name>     - buys a seat");
-        System.out.println("<date> <row>");
-        System.out.println("<seat>");
-        return content;
+        System.out.println("unbook <name> <date>     -> cancel a booked seat");
+        System.out.println("<row> <seat>");
+        System.out.println("buy <name> <date>        -> buys a seat");
+        System.out.println("<row> <seat>");
+        System.out.println("bookings [<name>]        -> checks the booked seats");
+        System.out.println("[<date>]");
+        System.out.println("check <code>             -> checks the code and return the row and seatNumber");
+        System.out.println("report <from>            -> checks the booked seats");
+        System.out.println("<to> [<hall>]");
+        System.out.println("mostviewed               -> shows the list of events ordered by most views");
+        return calendars;
     }
 }
