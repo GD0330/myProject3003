636374e2a36c0bb10946207256ccc96ec213ca61
GD0330 <161241619+GD0330@users.noreply.github.com>
2024-06-20 13:55:28 +0300

    Update Command.java

--- Command.java
+++ Command.java
@@ -1,7 +1,6 @@
 package bg.tu_varna.sit.a1.f22621658.fileCommands;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public interface Command {
     Calendars execute( String[] param, Calendars calendars) throws IOException;
