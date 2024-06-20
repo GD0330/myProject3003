eaa7fc68b085493247975fbedbe406a307a09aa0
GD0330 <161241619+GD0330@users.noreply.github.com>
2024-06-20 13:47:42 +0300

    Update OpenCommand.java

--- OpenCommand.java
+++ OpenCommand.java
@@ -4,11 +4,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class OpenCommand implements Command {
         @Override
-        public String execute(String filename, Scanner scanner, String content) throws IOException {
+        public Calendars execute( String[] param, Calendars calendars) throws IOException {
             File file = new File(filename);
             if (!file.exists()) {
                 if (file.createNewFile()) {
@@ -17,14 +16,23 @@ public class OpenCommand implements Command {
                     throw new IOException("Failed to create the file.");
                 }
             }
-            BufferedReader reader = new BufferedReader(new FileReader(filename));
-            String line;
-            StringBuilder rawContent = new StringBuilder();
-            while ((line = reader.readLine()) != null) {
-                rawContent.append(line);
+             if (file.length()>0) {
+                BufferedReader reader = new BufferedReader(new FileReader(filename));
+
+                String line;
+                StringBuilder rawContent = new StringBuilder();
+                while ((line = reader.readLine()) != null) {
+                    rawContent.append(line);
+                }
+                reader.close();
+
+                SimpleContentHandler simpleContentHandler = new SimpleContentHandler();
+                calendars = simpleContentHandler.decode(rawContent.toString());
+                System.out.println("File opened successfully.");
             }
-            reader.close();
-            System.out.println("File opened successfully.");
-            return rawContent.toString();
+            else{
+                System.out.println("Your file is empty !");
+            }
+            return calendars;
         }
 }
